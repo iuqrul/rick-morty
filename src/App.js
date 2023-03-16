bafea9a03a62b892759a8902e95065583c5b41f4
iuqrul <iuqrul@gmail.com>
2023-03-16 23:33:05 +0100

    Finalizado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import imageRickMorty from './img/rick-morty.png';
 import './App.css';
 import { useState } from 'react';
+import Characters from './components/Characters';
 
 function App() {
 
@@ -19,10 +20,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 className="title">Rick & Morty</h1>
-        <img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
-        <button onClick={reqApi} className="btn-search">
-          Buscar Personajes
-        </button>
+        
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters}/>
+        ) : (
+          <>
+            <img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
+            <button onClick={reqApi} className="btn-search">
+              Buscar Personajes
+            </button> 
+          </>
+        )}
+
       </header>
     </div>
   );
